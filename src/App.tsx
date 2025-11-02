4f631ff0faaa2a9a20d5d136edf4b75c57048eb3
preetidas60 <dasp69833@gmail.com>
2025-11-02 19:35:27 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+
+function App() {
+  const [alertVisible, setAlertVisible] = useState(false)
+  return (
+    <div>
+      {alertVisible && <Alert onClose={()=> setAlertVisible(false)}>My alert</Alert>}
+      <Button onClick={()=> setAlertVisible(true)}>
+        My button
+      </Button>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+
+// PascalCasing
+function Message(){
+    // JSX : Javascript XML
+    const name = "Preeti";
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
